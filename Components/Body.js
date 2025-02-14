51b2135478e2a2ae6eacd7a11cd84688aaba14e9
Selvarumugam <arumugamselva60@gmail.com>
2025-02-14 20:33:29 +0530

    AddedFilter , learned hooks and react reconcilation

--- Components/Body.js
+++ Components/Body.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import resobj from "../Utils/res-data";
 import RestaurentCard from "./RestaurentCard";
 const Body = () => {
+    const [RestaurentList,setRestaurentList] = useState(resobj);
     return (
         <div>
+            <div className="filter-btn">
+                <button onClick={()=> {
+                    let newRestaurentList = RestaurentList.filter((restaurent) => restaurent.info.rating.aggregate_rating > 4.5)
+                    setRestaurentList(newRestaurentList);
+                }}>Click me for good ones</button>
+            </div>
             <div className="res-container">
                 {
-                    resobj.map((mydata) => <RestaurentCard key={mydata.info.resId} resData = {mydata}></RestaurentCard>)
+                    RestaurentList.map((mydata) => <RestaurentCard key={mydata.info.resId} resData = {mydata}></RestaurentCard>)
                 }
             </div>
         </div>
